86c161e4b3dc85cecb5c611b8e2e9e9c52f0e5ac
aaron-lee-hebert <aaron.lee.hebert@gmail.com>
2025-04-26 06:50:24 -0500

    Create PowerPath.Web MVC Project
    Create a new project, `PowerPath.Web`, that will act as the main entry
    point for the application. It's configured with Microsoft Identity, EF
    Core, with a PostgreSQL database.
    
    Create a new entity, `User`, that inherits from `IdentityUser`, so that
    additional, custom fields may be added through the development cycle.

---
+++ PowerPath.Web/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PowerPath.Web.Data.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
